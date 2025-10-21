d53092699aae993cf0146f2c7d1cdee363ae65bc
rauldaza <rauld200116@gmail.com>
2025-10-21 14:00:49 -0500

    cambio mensaje app

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get('/hola', response_class=HTMLResponse)
 def get_hola():
-    return "<html><h1>hola Github actions<h1></html>"
+    return "<html><h1>hola Github actions, ahora con Continuous Deployment en Azure<h1></html>"
